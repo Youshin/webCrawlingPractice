2af2793a0f20cc21b1e62f5d22d72e147f51285f
Youshin Kim <youshin1411@gmail.com>
2020-06-02 14:18:21 +0900

    remove api key
    remove api key

--- day4/twitterJson.py
+++ day4/twitterJson.py
@@ -1,10 +1,10 @@
 import requests
 from requests_oauthlib import OAuth1
 
-consumer_key = 'H2alF8Q2p3liaAlaxDgPoGrJx'
-consumer_secret = 'odFi6NrEES4jwuXA3wmp9TVgPbFpGUHfnKidXpENIhwb2mK2H1'
-access_token = '1265033496512233472-kl5UoTqOVjVSfv5hG1QZL41Fqudslj'
-access_token_secret = 'kBLWiNWJ34j1xTzqUAzJgSCQvdCeAuVh8qCKuzAFZDcla'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 oauth = OAuth1(client_key=consumer_key, client_secret=consumer_secret, resource_owner_key=access_token, resource_owner_secret=access_token_secret)
 url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}'.format('naver_d2')
